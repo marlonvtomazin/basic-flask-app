5714db98e484cde5c164de92071bf16c0cf0e90a
marlonvt <marlon_kin@hotmail.com>
2025-10-18 17:11:25 -0300

    addes blueprint routes

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 from flask import Flask, url_for, render_template
+from routes.home import home_route
+from routes.cliente import cliente_route
 
 #inicializacao
 app = Flask(__name__)
 
-#rotas
-@app.route('/')
-def home():
-    titulo = 'Gestão de usuários'
-    usuarios = [
-        {"nome": "Marlon", "membro_ativo": True},
-        {"nome": "João", "membro_ativo": False},
-        {"nome": "Gabriela meu amor <3", "membro_ativo": False},
-    ]
-    return render_template('index.html', titulo=titulo, usuarios=usuarios)
-
-@app.route('/sobre')
-def pagina_sobre():
-    return """
-        <h1>Sobre Mim</h1>
-        <p>Meu nome é Marlon e esse é meu <a href="https://github.com/marlonvtomazin">Github</a>:  
-    """
+#registro de blueprints
+app.register_blueprint(home_route)
+app.register_blueprint(cliente_route, url_prefix='/clientes')
 
 
 #exe
