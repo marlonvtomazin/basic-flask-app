28fd12e473cdbc29b6b95af7285905d9ccbad30d
marlonvt <marlon_kin@hotmail.com>
2025-10-20 20:23:40 -0300

    Updated gitignore

--- database/cliente.py
+++
@@ -1,7 +0,0 @@
-CLIENTES = [
-    {"id": 1, "nome": "Marlon", "email": "marlonvtomazin@gmail.com"},
-    {"id": 2, "nome": "João", "email": "joaovtomazin@gmail.com"},
-    {"id": 3, "nome": "Gabriela", "email": "gabriela1@gmail.com"},
-    {"id": 4, "nome": "Pedro", "email": "pedro1@gmail.com"},
-    {"id": 5, "nome": "Maria", "email": "maria1@gmail.com"}
-]
